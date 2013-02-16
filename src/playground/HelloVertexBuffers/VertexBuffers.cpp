df2ff7713fc35105f1b7e8105366bee804099c61
Csaba Trucza <csaba.trucza@gmail.com>
2013-02-16 16:29:29 +0200

    drawing a dot in immediate and retained mode
    the first two steps:
    - draw a dot at (0,0,0) in immediate mode: just to show the minimal
    skeleton application
    - draw a dot at (0,0,0) with vertex buffer arrays to show retained mode
    skeleton

---
+++ src/playground/HelloVertexBuffers/VertexBuffers.cpp
@@ -0,0 +1,46 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+struct Point
+{
+    float x;
+    float y;
+};
+
+Point center;
+
+void init()
+{
+    center.x = 0.0f;
+    center.y = 0.0f;
+
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Point), &center, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+}
+
+void display()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glFlush();
+
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+	glutCreateWindow("Hello World!");
+	glutDisplayFunc(display);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    init();
+
+    glutMainLoop();
+}
---
+++ src/playground/playground/main.cpp
@@ -0,0 +1,22 @@
+#include <GL/freeglut.h>
+
+void display()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_POINTS);
+    glVertex2f(0.0f, 0.0f);
+    glEnd();
+
+    glFlush();
+
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+	glutCreateWindow("Hello World!");
+	glutDisplayFunc(display);
+	glutMainLoop();
+}
